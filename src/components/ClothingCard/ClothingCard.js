3e966314489bc9d87a5015301f0a4700d7775e1b
jzhzhang66 <jzhzhang66@gmail.com>
2022-04-01 09:20:51 -0400

    implemented hover states

--- src/components/ClothingCard/ClothingCard.js
+++ src/components/ClothingCard/ClothingCard.js
@@ -1,17 +1,20 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Card, Col } from "antd";
 import { ClothingCardContainer } from "./ClothingCard.style";
 
 const { Meta } = Card;
 
 const ClothingCard = ({ item }) => {
+  const [isHover, setIsHover] = useState(false);
   return (
     <ClothingCardContainer>
       <Col span={5}>
         <Card
           hoverable
           style={{ width: 240 }}
-          cover={<img src={item.image} alt="" />}
+          cover={<img src={isHover ? item.stat : item.image} alt="" />}
+          onMouseOver={() => setIsHover(true)}
+          onMouseOut={() => setIsHover(false)}
         >
           <Meta title={item.title} />
           {item.price}
